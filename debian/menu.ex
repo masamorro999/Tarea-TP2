a5984f8898eaf981de5f43a7c2a756fb442636ad
Mateo Sierra <mateo.sierra@c3.itm.edu.co>
2016-09-22 20:03:09 +0000

    Primer commit luego de haber debianizado

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(testpackage):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="testpackage" command="/usr/bin/testpackage"
---
+++ debian/testpackage.default.ex
@@ -0,0 +1,10 @@
+# Defaults for testpackage initscript
+# sourced by /etc/init.d/testpackage
+# installed at /etc/default/testpackage by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
