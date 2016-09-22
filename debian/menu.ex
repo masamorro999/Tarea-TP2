8bd3f81037b6a24dbd37d2a55580091e7f65c97a
Mateo Sierra <mateo.sierra@c3.itm.edu.co>
2016-09-22 20:10:23 +0000

    commit luego del empaquetamiento al directorio debian

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(testpackage):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="testpackage" command="/usr/bin/testpackage"
--- debian/testpackage.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the testpackage package
-#
-0 4	* * *	root	[ -x /usr/bin/testpackage_maintenance ] && /usr/bin/testpackage_maintenance
